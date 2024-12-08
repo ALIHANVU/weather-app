7ecbfae589095e7dae1a5dac5388887d6d1b3938
ALIHANVU <alihan95997@gmail.com>
2024-12-09 02:00:53 +0300

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,5 +1,10 @@
-const toggleTheme = () => {
-    isDarkTheme = !isDarkTheme;
-    document.body.classList.toggle('dark-theme', isDarkTheme);
-    themeToggle.textContent = isDarkTheme ? '☀️' : '🌙';
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('themeToggle');
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-theme');
+        document.querySelector('.app-container').classList.toggle('dark-theme');
+        Array.from(document.querySelectorAll('input, button')).forEach(element => {
+            element.classList.toggle('dark-theme');
+        });
+    });
+});
